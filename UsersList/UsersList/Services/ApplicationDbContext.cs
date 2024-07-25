03c8c257c7a5d72ac8e7d6f0ae8c172b343e8b38
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-07-25 20:59:02 +0200

    add first test migration

--- UsersList/UsersList/Services/ApplicationDbContext.cs
+++ UsersList/UsersList/Services/ApplicationDbContext.cs
@@ -5,11 +5,20 @@ namespace UsersList.Services
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-            
+
         }
-        
-        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "Andrzej"},
+                new User { Id = 2, Name = "Janusz" }
+                );
+        }
     }
 }
