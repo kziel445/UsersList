527f8e07d31c6f6ad34866397ab5139baa6efaed
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-07-25 08:22:34 +0200

    add database context with basic user model

---
+++ UsersList/UsersList/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace UsersList.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ UsersList/UsersList/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersList.Models;
+
+namespace UsersList.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<User> Users { get; set; }
+    }
+}
