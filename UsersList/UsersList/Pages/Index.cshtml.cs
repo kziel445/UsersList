7862ade8aea902af94a6dfdf19e1eb9d25fb171d
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-07-29 20:19:08 +0200

    add list on index site

--- UsersList/UsersList/Pages/Index.cshtml.cs
+++ UsersList/UsersList/Pages/Index.cshtml.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using UsersList.Models;
+using UsersList.Services;
+using System.Threading.Tasks;
 
 namespace UsersList.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public IList<User> Users { get; set; }
+        public async Task OnGetAsync()
         {
-
+            if(_context.Users != null)
+            {
+                Users = await _context.Users.ToListAsync();
+            }
         }
     }
 }
