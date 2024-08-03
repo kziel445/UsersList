f6b3187ba842b81b044524c5380ec4aa9308ad09
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-08-03 10:02:00 +0200

    extend user model

---
+++ UsersList/UsersList/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UsersList.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<SubCategories> SubCategories { get; set; }
+    }
+}
--- UsersList/UsersList/Models/User.cs
+++ UsersList/UsersList/Models/User.cs
@@ -1,8 +1,16 @@
-﻿namespace UsersList.Models
+﻿using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace UsersList.Models
 {
     public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
-    }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Category Category { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
 }
