3a1d5a1ce2c47c9c2e870cf46ada8aea59a26519
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-08-06 21:35:16 +0200

    add simple authentication
    roles not included

---
+++ UsersList/UsersList/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UsersList.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
---
+++ UsersList/UsersList/Services/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UsersList.Models;
+
+namespace UsersList.Services
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
